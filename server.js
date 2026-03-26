658a85321c13ab6972012563bc55d3d766017632
Arthur Onyeanusi <arthuronyeanusi@gmail.com>
2026-03-26 08:44:56 +0100

    Succesfully created a simple chat app, basic model stripped of every component, just pure logic

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import { createServer } from "http";
-import WebSocket from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 
 const host = "localhost";
 const port = 5000
@@ -9,14 +9,76 @@ const server = createServer((req, res)=>{
     res.end("Hello World\n")
 });
 
-// const {ws1, ws2} = new WebSocket();
 
-// const connectedUsers = {
-//     "Arthur": [ws1],
-//     "Nelson": [ws2]
-// }
+const wss = new WebSocketServer({ server });
 
+
+const connectedUsers = {}
 const offlineMessages = {}
+
+wss.on("connection", (ws, req) => {
+    const reqUrl = new URL(req.url, `http://${req.headers.host}`);
+    ws.userId = (reqUrl.searchParams.get("userId") || "unknown").trim();
+    const userId = ws.userId;
+
+    if (!connectedUsers[userId]) connectedUsers[userId] = [];
+    connectedUsers[userId].push(ws);
+
+    console.log("Connected Users:", Object.keys(connectedUsers));
+
+
+    if (offlineMessages[userId] && offlineMessages[userId].length > 0) {
+        offlineMessages[userId].forEach(msg => ws.send(JSON.stringify(msg)));
+        offlineMessages[userId] = [];
+    }
+
+    ws.on("message", (data)=>{
+        try {
+            const parsed = JSON.parse(data);
+            handleMessage(parsed, ws)
+        } catch (error) {
+            console.log("Invalid Message Format");
+            
+        }
+    })
+
+    ws.on("close", ()=>{
+        console.log(`${userId} is disconnected`);
+        connectedUsers[userId] = connectedUsers[userId].filter(conn => conn !== ws);
+
+        if (connectedUsers[userId].length === 0){
+            delete connectedUsers[userId];
+            console.log("Updated connected users: ", Object.keys(connectedUsers));
+            
+        }
+        
+    })
+    
+})
+
+function handleMessage(message, ws) {
+    const { type, to, content } = message;
+    const from = ws.userId;
+
+    if (type === "message") {
+        const msgObj = {
+            id: Date.now().toString(),
+            from,
+            to,
+            content,
+            timestamp: Date.now()
+        };
+
+        const receivers = connectedUsers[to];
+
+        if (receivers && receivers.length > 0) {
+            receivers.forEach(conn => conn.send(JSON.stringify(msgObj)));
+        } else {
+            if (!offlineMessages[to]) offlineMessages[to] = [];
+            offlineMessages[to].push(msgObj);
+        }
+    }
+}
 
 
 server.listen(port, host,()=>{
