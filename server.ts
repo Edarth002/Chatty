538f68f52a340f8586563b77cf1d9bed15266f1f
Arthur Onyeanusi <arthuronyeanusi@gmail.com>
2026-04-04 08:05:38 +0100

    Completed flow from authentication to connection and validating users, up till sending messages

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
 import http from "http";
 import express from "express";
+import cors from "cors";
+import { setupWebSocket } from "./app/chatRoute.ts";
 import loginRoute from "./authRoutes/loginRoute.ts";
 import registerRoute from "./authRoutes/registerRoute.ts";
 import meRoute from "./authRoutes/meRoute.ts";
 import usersRoute from "./authRoutes/usersRoute.ts";
-import cors from "cors";
 
 const app = express();
 const port = 4000;
@@ -17,14 +18,14 @@ app.use("/api", registerRoute);
 app.use("/api", meRoute);
 app.use("/api", usersRoute);
 
-
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
+const server = http.createServer(app);
 
-const server = http.createServer(app);  
+setupWebSocket(server); // <-- attach WS to the same server
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
